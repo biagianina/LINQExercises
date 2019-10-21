4874e4f2361339b92532a3e8f74f087dc26eae62
biagianina <53540842+biagianina@users.noreply.github.com>
2019-10-21 18:18:22 +0300

    consonants and vowels counter

---
+++ LinqExercises/CountVowelsAndConsonants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExercises
+{
+    public class CountVowelsAndConsonants
+    {
+        readonly string input;
+        readonly string vowels = "aeiou";
+
+        public CountVowelsAndConsonants(string input)
+        {
+            this.input = input;
+        }
+
+        public (int, int) VowelsAndConsonantsCounter()
+        {
+            var result = (0, 0);
+            result.Item1 = input.Aggregate(0, (count, c) =>
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+                else if (char.IsLetter(c))
+                {
+                    result.Item2++;
+                }
+
+                return count;
+            });
+            return result;
+        }
+    }
+}
---
+++ LinqExercisesTests/CountVowelsAndConsonantsTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace LinqExercises
+{
+    public class CountVowelsAndConsonantsTests
+    {
+        [Fact]
+        public void VowelsNumber()
+        {
+            var text = new CountVowelsAndConsonants("ana are mere");
+            Assert.Equal((6, 4), text.VowelsAndConsonantsCounter());
+        }
+    }
+}
