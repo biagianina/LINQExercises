e586cc79bc8692d52e6d982ba406a41bb49b47b9
biagianina <53540842+biagianina@users.noreply.github.com>
2019-11-04 11:24:31 +0200

    Implemented maximum occurances counter

---
+++ LinqExercises/MaximumCharacterOccurances.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqExercises
+{
+    public class MaximumCharacterOccurances
+    {
+        readonly string input;
+
+        public MaximumCharacterOccurances(string input)
+        {
+            this.input = input;
+        }
+
+        public char GetCharacterWithMaximumOccurances()
+        {
+            var occurancesCounter = input.Aggregate(new Dictionary<char, int>(), (occurances, c) =>
+            {
+                if (occurances.TryGetValue(c, out int numberOfOccurances))
+                {
+                    occurances[c]++;
+                }
+                else
+                {
+                    occurances.Add(c, 1);
+                }
+
+                return occurances;
+            });
+
+            return occurancesCounter.Select(c => c).Aggregate(new KeyValuePair<char, int>(), (max, c) =>
+            max.Value > c.Value ? max : c).Key;
+        }
+    }
+}
---
+++ LinqExercisesTests/MaximumCharacterOccurancesTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace LinqExercises
+{
+    public class MaximumCharacterOccurancesTests
+    {
+        [Fact]
+        public void GetCharacterFromOneCharacterInput()
+        {
+            var input = new MaximumCharacterOccurances("a");
+            Assert.Equal('a', input.GetCharacterWithMaximumOccurances());
+        }
+
+        [Fact]
+        public void GetCorrectCharacterFromInput()
+        {
+            var input = new MaximumCharacterOccurances("aaabbbbcddddd453");
+            Assert.Equal('d', input.GetCharacterWithMaximumOccurances());
+        }
+    }
+}
