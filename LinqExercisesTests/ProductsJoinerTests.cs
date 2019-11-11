6a3d9b09cdce8c59278964a199ece98db8b42fc2
biagianina <53540842+biagianina@users.noreply.github.com>
2019-11-11 13:49:15 +0200

    refactored tests for joined lists of products

--- LinqExercisesTests/ProductsJoinerTests.cs
+++ LinqExercisesTests/ProductsJoinerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace LinqExercises
@@ -51,7 +50,57 @@ namespace LinqExercises
                 owen
             };
 
-            Assert.Equal(expected, result.JoinProducts());
+            Equals(expected, result.JoinProducts());
+        }
+
+        [Fact]
+        public void MultipleProductsJoined()
+        {
+            var laptop = new Prod { Name = "laptop", Quantity = 10 };
+            var phone = new Prod { Name = "mobile phone", Quantity = 5 };
+            var tv = new Prod { Name = "TV", Quantity = 6 };
+            var console = new Prod { Name = "console", Quantity = 7 };
+            var microvawe = new Prod { Name = "microwave", Quantity = 11 };
+            var mixer = new Prod { Name = "mixer", Quantity = 5 };
+            var shaker = new Prod { Name = "shaker", Quantity = 6 };
+            var refrigerator = new Prod { Name = "refrigerator", Quantity = 7 };
+            var owen = new Prod { Name = "owen", Quantity = 11 };
+            var laptop2 = new Prod { Name = "laptop", Quantity = 20 };
+            var phone2 = new Prod { Name = "mobile phone", Quantity = 10 };
+            var list1 = new List<Prod>
+            {
+                laptop,
+                phone,
+                tv,
+                console,
+                microvawe
+            };
+
+            var list2 = new List<Prod>
+            {
+                laptop,
+                phone,
+                mixer,
+                shaker,
+                refrigerator,
+                owen
+            };
+
+            var result = new ProductsJoiner(list1, list2);
+            var expected = new List<Prod>
+            {
+                laptop2,
+                phone2,
+                tv,
+                console,
+                microvawe,
+                mixer,
+                shaker,
+                refrigerator,
+                owen
+            };
+
+            Equals(expected, result.JoinProducts());
         }
     }
 }
